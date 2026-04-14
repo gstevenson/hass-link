aff59462211a293cfd8f4c3ecedeefc2c9b13a5f
Greg Stevenson <greg@stevenson.gs>
2026-04-14 20:58:36 +0100

    feat: show elevation status in About and Sensors UI
    Surfaces whether the app is running as Administrator in two places:
    - About form: status line below OS version (green/orange)
    - Sensors tab: dynamic note replacing the static warning, so users
      know immediately whether temperature sensors will actually work

--- src/HassLink/Forms/AboutForm.cs
+++ src/HassLink/Forms/AboutForm.cs
@@ -1,3 +1,5 @@
+using System.Security.Principal;
+
 namespace HassLink.Forms;
 
 public class AboutForm : Form
@@ -5,7 +7,7 @@ public class AboutForm : Form
     public AboutForm()
     {
         Text = "About hass-link";
-        Size = new Size(360, 220);
+        Size = new Size(360, 240);
         FormBorderStyle = FormBorderStyle.FixedDialog;
         MaximizeBox = false;
         MinimizeBox = false;
@@ -23,6 +25,15 @@ public class AboutForm : Form
         panel.Controls.Add(new Label { Text = $"Version {Application.ProductVersion}", AutoSize = true, Padding = new Padding(0, 4, 0, 0) });
         panel.Controls.Add(new Label { Text = "Windows → Home Assistant via MQTT", AutoSize = true, Padding = new Padding(0, 8, 0, 0), ForeColor = Color.Gray });
         panel.Controls.Add(new Label { Text = $"Running on {Environment.OSVersion}", AutoSize = true, Padding = new Padding(0, 4, 0, 0), ForeColor = Color.Gray });
+
+        var isAdmin = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+        panel.Controls.Add(new Label
+        {
+            Text = isAdmin ? "Running as Administrator" : "Not running as Administrator",
+            ForeColor = isAdmin ? Color.DarkGreen : Color.DarkOrange,
+            AutoSize = true,
+            Padding = new Padding(0, 4, 0, 0),
+        });
 
         var link = new LinkLabel { Text = "github.com/gstevenson/hass-link", AutoSize = true, Padding = new Padding(0, 12, 0, 0) };
         link.LinkClicked += (_, _) =>
